a942bcde15a5d74cf71ff2bd5803b596b9c55a46
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2020-10-14 16:34:31 +0200

    make into console application

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,56 +7,71 @@ import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
 
 public class Main {
+    private static Scanner _scanner = new Scanner(System.in);
+    private static Peer _peer;
+    private static int _portNumber;
 
     public static void main(String[] args) {
-        Peer peer;
+
+        System.out.print("Please enter a port number: ");
+        _portNumber = Integer.parseInt(readLine().trim());
         try {
-            peer = new Peer(7777);
+            _peer = new Peer(_portNumber);
         } catch (IOException e) {
-            e.printStackTrace();
-            return;
+            System.err.println("Failed to establish server");
+            System.exit(-1);
         }
+        _peer.setEstablishConnectionEvent(Main::onPeerReceiveConnection);
+        menu();
+    }
 
-        peer.setEstablishConnectionEvent(Main::onPeerReceiveConnection);
-
+    private static void menu() {
         while (true) {
-            System.out.println("Listening on specified port...");
-            try {
-                peer.awaitConnection();
-            } catch (IOException e) {
-                e.printStackTrace();
-                break;
+            System.out.println("What do you want to do?");
+            System.out.println("[0]: Await message");
+            System.out.println("[1]: Transmit message");
+            System.out.println("[2]: Exit");
+            switch (readLine()) {
+                case "0" -> {
+                    System.out.printf("Awaiting input on port '%s'...\n", _portNumber);
+                    try {
+                        _peer.awaitConnection();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                case "1" -> {
+                    System.out.print("Please enter a message to transmit: ");
+                    String message = readLine();
+                    System.out.print("Please enter IP: ");
+                    String ipString = readLine().trim();
+                    System.out.print("Please enter port number: ");
+                    int portNumber = Integer.parseInt(readLine().trim());
+                    try {
+                        _peer.sendMessage(message, ipString, portNumber);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                case "2" -> System.exit(0);
+                default -> System.out.println("You can only enter integers.");
             }
         }
     }
 
     private static void onPeerReceiveConnection(EstablishConnectionEventArgs eventArgs) {
-        Peer receiver = eventArgs.getServer();
         Socket sender = eventArgs.getClient();
         try (InputStream inputStream = sender.getInputStream()) {
-            System.out.printf("Receive data from sender -> %s:%s\n", sender.getInetAddress().getHostAddress(), sender.getPort());
-            /*DataInputStream dataInputStream = new DataInputStream(inputStream);
-            String message = dataInputStream.readUTF();
-            System.out.printf("Received message: %s\n", message);*/
-            byte[] messageLengthBytes = new byte[4];
-            inputStream.read(messageLengthBytes, 0, 4);
-            int messageLength = (((messageLengthBytes[3] & 0xff) << 24) | ((messageLengthBytes[2] & 0xff) << 16) |
-                      ((messageLengthBytes[1] & 0xff) << 8) | (messageLengthBytes[0] & 0xff));
-            byte[] messageBuffer = new byte[messageLength];
-            inputStream.read(messageBuffer, 0, messageLength);
-            String message = new String(messageBuffer, 0, messageLength);
-            System.out.printf("Received message: %s\n", message);
+            System.out.printf("A connection was established with %s:%s\n", sender.getInetAddress().getHostAddress(), sender.getPort());
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+            String receivedMessage = dataInputStream.readUTF();
+            System.out.printf("Received message: %s\n", receivedMessage);
         } catch (IOException e) {
-            System.err.println("Could not get input stream");
+            e.printStackTrace();
         }
+    }
 
-        String senderIp = sender.getInetAddress().getHostAddress();
-        int senderPort = sender.getPort();
-
-        try {
-            receiver.sendMessage("Your message was received!", senderIp, senderPort);
-        } catch (IOException e) {
-            System.err.println("Could not send confirmation message");
-        }
+    private static String readLine() {
+        return _scanner.nextLine();
     }
 }
